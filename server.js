b4519e7a7d380d0880066f958fe8fff792e67a5e
akhil.antony <akhil.antony@cubettech.com>
2018-07-17 15:04:04 +0530

    initial commit: gets node server up and runnig!

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const hostName = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('say node...\n');
+});
+
+server.listen(port, hostName, () => {
+  console.log(`node running at http://${hostName}:${port}`);
+});
