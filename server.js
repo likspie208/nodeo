f65615212714f7508c7031dca6b2632c0599d780
Akhil Antony K <antonykakhil@gmail.com>
2018-12-28 15:11:53 +0530

    fix charset issue for firefox

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.end('say node...\n');
 });
 
